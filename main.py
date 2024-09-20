4a2d3f3a09e80bfb4d36867aa5b8140f33e6cae1
Maple <2281683038@qq.com>
2024-09-20 19:57:11 +0800

    can work now

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import pyperclip
 import subprocess
 
 
-def copy_to_clipboard(url):
-    pyperclip.copy(url)
-
-
 def get_device_ip():
     res = subprocess.run(
         "ipconfig",
@@ -26,25 +22,25 @@ def display_url(url):
     url_label = tk.Label(root, text=url, wraplength=800, justify="center")
     url_label.pack(pady=10)
 
-    copy_button = tk.Button(root, text="复制", command=lambda: copy_to_clipboard(url))
+    copy_button = tk.Button(root, text="复制", command=pyperclip.copy(url))
     copy_button.pack(pady=10)
     root.mainloop()
 
 
 def main():
     # 如果发生错误, 请更换端口
-    port = 8371
+    port = 8861
     ip = get_device_ip()
     url = f"http://{ip}:{port}"
 
-    server = subprocess.Popen(
+    http_server = subprocess.Popen(
         f"python -m http.server {port}",
         stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
     )
 
     display_url(url)
 
-    server.terminate()
+    http_server.terminate()
 
     print("程序已关闭")
 
